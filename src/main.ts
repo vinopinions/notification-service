b4a6ce31cde3fae6f844d9baec257e608873b6a0
oskar <ooskar@gmx.de>
2024-05-06 11:36:56 +0200

    feat: create microservice

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const configService: ConfigService = new ConfigService();
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.REDIS,
       options: {
-        host: 'localhost',
-        port: 6379,
+        host: configService.getOrThrow('REDIS_HOSTNAME'),
+        port: configService.getOrThrow('REDIS_PORT'),
       },
     },
   );
